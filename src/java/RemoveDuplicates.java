b523ae57d9bc673c33f54885298fb1553549f31e
srikantheslavath <srikantheslavath6@gmail.com>
2023-05-23 09:27:32 -0700

    programs

--- src/java/RemoveDuplicates.java
+++ src/java/RemoveDuplicates.java
@@ -1,16 +1,17 @@
+package java;
+
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 
 public class RemoveDuplicates {
+
     public static void main(String[] args) {
         String s= "aaabkdoodd";
         HashSet<Character> h=new HashSet<>();
         for(int i=0;i<s.length();i++) {
             h.add(s.charAt(i));
         }
-            for(Character a:h){
-                System.out.println(a);
-            }
+        for(Character a:h){
+            System.out.println(a);
         }
     }
-
+}
